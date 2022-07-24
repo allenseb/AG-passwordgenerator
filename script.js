5bab0454c6e4b3a1eb2678a6887e5d4ee4a5e7b7
allenseb <allenseb38@gmail.com>
2022-07-24 12:54:00 -0700

    addingfiles

--- script.js
+++ script.js
@@ -6,12 +6,50 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
+  let passwordLength = getPasswordLength()
+  if (passwordLength >= 8 && passwordLength <= 128) {
+    var password = generatePassword(passwordLength);
+    var passwordText = document.querySelector("#password");
+    passwordText.value = password;
+  }
+  else {
+    alert('password was not within length criteria');
+  }
+ 
 
 }
+
+function generatePassword(passwordLength) {
+  var password = ""
+  var characters = '';
+  var upperCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  var lowerCase = 'abcdefghijklmnopqrstuvwxyz';
+  var numbers = '0123456789';
+
+  if (document.getElementById("checkbox1").checked) {
+    characters += upperCase
+  }
+  if (document.getElementById("checkbox2").checked) {
+    characters += lowerCase
+  }
+  if (document.getElementById("checkbox3").checked) {
+    characters += numbers
+  }
+
+  for (var i = 0; i < passwordLength; i++) {
+    var randomInteger = Math.floor(Math.random() * characters.length)
+    password += characters.charAt(randomInteger)
+  }
+
+  return password
+}
+
+function getPasswordLength() {
+ let userInput = prompt("Please enter password length") 
+ let passwordLength = parseInt(userInput);
+ return passwordLength
+}
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
